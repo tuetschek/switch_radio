c98408d2e85672bf136c2b8488ad42e3eff227c7
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2020-01-19 21:31:27 +0100

    Czech radio simplification

--- czech_radio.py
+++ czech_radio.py
@@ -8,55 +8,17 @@ from flask import Flask, json
 from flask_ask import Ask, session, statement, audio, current_stream
 
 app = Flask(__name__)
-app.config['ASK_VERIFY_REQUESTS'] = False
-ask = Ask(app, "/switch_radio")
+ask = Ask(app, "/czech_radio")
 logger = logging.getLogger('flask_ask')
 logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
-DB_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'switch_radio.sqlite')
 
-STREAMS = {
-    'czech radio one': 'https://api.play.cz/radio/cro1-128.mp3.m3u',
-    'czech radio two': 'https://api.play.cz/radio/cro2-128.mp3.m3u',
-    'czech radio three': 'https://api.play.cz/radio/cro3-128.mp3.m3u',
-    'czech radio plus': 'https://api.play.cz/radio/croplus128.mp3.m3u',
-    'NDR culture': 'https://www.ndr.de/resources/metadaten/audio/m3u/ndrkultur.m3u',
-    'beat': 'https://api.play.cz/radio/beat128.mp3.m3u',
-    'BBC one': 'http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p.m3u',
-    'BBC two': 'http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio2_mf_p.m3u',
-}
-DEFAULT_STATION = list(STREAMS.keys())[0]
+STREAM = 'https://api.play.cz/radio/cro1-128.mp3.m3u'
 
 
 @ask.launch
 def launch():
-    db = sqlite3.connect(DB_FILE)
-    c = db.cursor()
-    c.execute('SELECT * FROM stations WHERE uid = ?', (session.user.userId, ))
-    station = c.fetchall()
-    if not station:
-        station = DEFAULT_STATION
-        logger.info('INS uid = %s station = %s' % (session.user.userId, station))
-        c.execute('INSERT INTO stations(uid, station) VALUES (?, ?)', (session.user.userId, station))
-        db.commit()
-    else:
-        station = station[0][1]
-    db.close()
-    return audio().play(STREAMS[station])
-
-
-@ask.intent('Tune', mapping={'station': 'station'})
-def tune(station):
-    if station not in STREAMS:
-        return statement('I\'m sorry, I don\'t know the station "%s"' % station)
-
-    db = sqlite3.connect(DB_FILE)
-    c = db.cursor()
-    logger.info('UPD uid = %s station = %s' % (session.user.userId, station))
-    c.execute('UPDATE stations SET station = ? WHERE uid = ?', (station, session.user.userId))
-    db.commit()
-    db.close()
-    return audio().play(STREAMS[station])
+    return audio().play(STREAM)
 
 
 @ask.intent('AMAZON.PauseIntent')
@@ -110,11 +72,4 @@ def _infodump(obj, indent=2):
 
 
 if __name__ == '__main__':
-    # create database
-    if not os.path.isfile(DB_FILE):
-        db = sqlite3.connect(DB_FILE)
-        c = db.cursor()
-        c.execute('CREATE TABLE stations (uid varchar(256), station varchar(256))')
-        db.commit()
-        db.close()
-    app.run(host='0.0.0.0')#ssl_context=('certificate.crt', 'private.key'), port=4443, host='0.0.0.0')
+    app.run(host='127.0.0.1', port=5001)
